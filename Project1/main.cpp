a3dc5a6e918cb52c7dd31ec7285bf6333f2dbbf3
VorobyevVladimir <vladimirv15012010@gmail.com>
2025-04-07 17:51:48 +0300

    use pointer to work with obj , dynamic memory

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -9,36 +9,23 @@ void change(Student st) {
 
 int main() {
 
-	Student st1, temp;
+	Student *st1 = nullptr;
+	st1 = new Student;
 
-	st1.name = "Bogdan";
-	st1.age = 14;
-	st1.mark = 9.5;
-	st1.alive = true;
+	Student* st2 = st1;
 
-	temp = st1;
+	st1->name = "Vlad";
+	st1->age = 15;
+	st1->mark = 9.9;
+	st1->alive = true;
 
+	cout << "Before: " << endl;
+	cout << st1->getString() << endl;
 
-	cout << "Before : " << endl;
-	cout<< st1.getString() << endl;
-	cout << temp.getString() << endl;
+	st2->name = "Matvey";
 
-	//change(st1);
-	st1.name = "Vova";
-
-	cout << "After : " << endl;
-	cout << st1.getString() << endl;
-	cout << temp.getString() << endl;
-
-	//cout << st1.getString() << endl;
-	//cout << st2.getString() << endl;
-	//cout << st3.getString() << endl;
-	
-	//string name = st1.mark > st2.mark ? st1.name : st2.name;
-
-	//Student t = st1.mark > st2.mark ? st1 : st2;
-
-	//cout << "Best : " << t.name << endl;
+	cout << "After: " << endl;
+	cout << st1->getString() << endl;
 
 	return 0;
 }
