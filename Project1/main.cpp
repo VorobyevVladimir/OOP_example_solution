baeeb6f9dcf1be27db6c9290a42ddb22abe8d5b8
VorobyevVladimir <vladimirv15012010@gmail.com>
2025-04-09 17:59:41 +0300

    constructor and destruxcctor

--- Project1/main.cpp
+++ Project1/main.cpp
@@ -12,6 +12,7 @@ int main() {
 	Student student3{ "Nikita", 15, 8.0, true};
 	Student student4{ student3 };
 
+
 	cout << student1.toString() << endl;
 	cout << student2.toString() << endl;
 	cout << student3.toString() << endl;
